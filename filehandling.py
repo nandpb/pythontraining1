6779be1b13402e3d1560087773537b4bf84ff3c2
nandpb <34053350+nandpb@users.noreply.github.com>
2017-12-01 12:21:34 +0530

    filehandling

--- filehandling.py
+++ filehandling.py
@@ -1,30 +1,40 @@
-file1 = open("file.txt","r")
+"""
+try:
+    file1 = open("file.txt","r")
+except (FileNotFoundError):
+    print("file not found ")
 
 def no_line_file(file1):
-    """This function is to find the total no.of line contain in the file"""
+    #This function is to find the total no.of line contain in the file
     count = 0
     for line in file1:
         count += 1
         list2 = line.split()
     return count
-
-
+    
 rows = no_line_file(file1)
+"""
 
-file1 = open("file.txt","r")
 avg = 0.0 # Average value of second column in list
 total = 0 # Total value of second column in list
 count = 0 # To count Number of lines in the file
+
+try:
+    file1 = open("file.txt","r")
+
+except FileNotFoundError:
+    print("File not found")
+
 for line in file1:
     count += 1
     words = line.split()
+
     try:
         words[1]=float(words[1])
         total += words[1]
 
     except (ValueError, TypeError):
         print("could not convert String to float")
-
     avg = total/count
 
 
