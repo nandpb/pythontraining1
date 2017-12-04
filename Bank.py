a5d03ec6e2477992f0703b915a3148044fdd7dc1
nandpb <34053350+nandpb@users.noreply.github.com>
2017-12-04 10:51:45 +0530

    Bank class

--- Bank.py
+++ Bank.py
@@ -1,34 +1,116 @@
-class BankAccount:
+class Account:
+    cust_name=""
+    acc_no=0
+    acc_type=""
 
-    min_balance = 1500;
+    def get_accinfo(self):
+        cust_name = input("Enter Customer Name : ")
+        try:
+            acc_no = int(input("Enter Account Number : "))
+        except:
+            print("enter only Numbers String special char Can't accept here ")
+        acc_type = input("enter account type : ")
 
 
-    def withdraw(self, amount, balance):
-        self.balance = balance
-        return (balance-amount)
+    def display_info(self,cust_name,acc_no,acc_type):
+        print("Customer Name : ", cust_name)
+        print("Account number : ", acc_no)
+        print("account type : ", acc_type)
 
-    def deposit(self, amount, balance):
-        self.balance = balance
-        return (balance+amount)
 
-print("choose your choice \n1.withdraw \n2.deposti ")
-choise = input("enter your choice :")
+class Cur_Acc (Account):
+    balance = 0.0
+    def display_currbal(self):
+        print("Balance : ",self.balance)
 
-if choise == '1':
-    amount = int(input("enter an amount to Withdraw : "))
-    if amount>0:
-        ba = BankAccount()
-        if ba.min_balance>=1000:
-            print(ba.withdraw(amount,ba.min_balance))
+    def deposit_currbal(self):
+        deposit =  int(input("enter amount to deposit : "))
+        self.balance += deposit
+
+    def withdraw_currbal(self):
+        print("Balance : ",self.balance)
+        withdraw = int(input("Enter amount to be withdraw : "))
+        self.balance -= withdraw
+        if self.balance<500:
+            penalty = (500-self.balance)/10
+            self.balance -= penalty
+            print("Balance after deducting penalty : ",self.balance)
+        elif self.balance<500:
+            print("\n you have to take permission for bank overdraft Facility : \n")
+            self.balance += withdraw
         else:
-            print("you can\'t withdraw this amount your balance exceed to min balance ")
-    else:
-        print("the amount should grater than Zero")
+            print("After withdraw your balance revels : ",self.balance)
 
-if choise == '2':
-    amount = int(input("enter an amount to Withdraw : "))
-    if amount > 0:
-        ba = BankAccount()
-        print(ba.deposit(amount, ba.min_balance))
+
+class Sav_Acc (Account):
+    savbal = 100.0
+    def disp_savbal(self):
+        print("balance : ",self.savbal)
+
+    def deposit_savbal(self):
+        deposit=int(input("Enter your amount to Deposit : "))
+        self.savbal += deposit
+        interest = (self.savbal*2)/100
+        self.savbal += interest
+
+    def withdraw_savbal(self):
+        print("Balance : ",self.savbal)
+        withdraw = int(input("Enter amount to be withdraw : "))
+        self.savbal -= withdraw
+        if(withdraw>self.savbal):
+            print(" Your have to take permission for Bank Overdraft Facility \n")
+            self.savbal += withdraw
+        else:
+            print("After withdraw your balance revels : ",self.savbal)
+
+
+class Bank:
+    c1 = Cur_Acc()
+    s1 = Sav_Acc()
+    char = input("Enter S for saving a/c customer C for current a/c customer : ")
+
+    if (char=="S" or char=="C"):
+        s1.get_accinfo()
+        while (1):
+
+            choice =input("1. Deposit \n2. Withdraw \n3.Display Balnace \n4.Display with full details \n5.Exit \n6.choose your choice\n")
+            print("choose your choise :\n")
+            if choice=='1':
+                s1.deposit_savbal()
+                break
+            elif choice=='2':
+                s1.withdraw_savbal()
+                break
+            elif choice=='3':
+                s1.disp_savbal()
+                break
+            elif choice=='4':
+                s1.display_info()
+                break
+            elif choice=='5':
+                exit()
+            else:
+                print("Entered chois is invalid, \"try again\"")
     else:
-        print("the amount should grater than Zero")
+        c1.get_accinfo()
+        while (1):
+
+            choice =input("1. Deposit \n2. Withdraw \n3.Display Balnace \n4.Display with full details \n5.Exit \n6.choose your choice\n")
+            print("choose your choise :\n")
+            if choice=='1':
+                s1.deposit_savbal()
+                break
+            elif choice=='2':
+                s1.withdraw_savbal()
+                break
+            elif choice=='3':
+                s1.disp_savbal()
+                break
+            elif choice=='4':
+                s1.display_info()
+                break
+            elif choice=='5':
+                exit()
+            else:
+                print("Entered chois is invalid, \"try again\"")
+
